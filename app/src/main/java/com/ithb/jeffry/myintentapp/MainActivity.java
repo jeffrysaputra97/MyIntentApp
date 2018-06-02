696b0786ce020de2f02f680d4694fac1195946ce
Jeffry Saputra <jeffrysaputra97@yahoo.com>
2018-06-02 21:17:28 +0700

    Menambahkan button untuk menggunakan intent implicit dan berharap nilai balik dari perpindahan activity

--- app/src/main/java/com/ithb/jeffry/myintentapp/MainActivity.java
+++ app/src/main/java/com/ithb/jeffry/myintentapp/MainActivity.java
@@ -6,12 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnMoveActivity;
     private Button btnMoveWithDataActivity;
     private Button btnMoveWithObject;
     private Button btnDialPhone;
+    private Button btnMoveForResult;
+    private TextView tvResult;
+    private int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnDialPhone = findViewById(R.id.btn_dial_number);
         btnDialPhone.setOnClickListener(this);
+
+        btnMoveForResult = findViewById(R.id.btn_move_for_result);
+        btnMoveForResult.setOnClickListener(this);
+
+        tvResult = findViewById(R.id.tv_result);
     }
 
     @Override
@@ -63,6 +74,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
                 startActivity(dialPhoneIntent);
                 break;
+
+            case R.id.btn_move_for_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+                startActivityForResult(moveForResultIntent, REQUEST_CODE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == MoveForResultActivity.RESULT_CODE){
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText("Hasil: " + selectedValue);
+            }
         }
     }
 }
